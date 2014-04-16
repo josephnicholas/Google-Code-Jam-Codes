790b8d505404efe1b1f2c170770113e4e70efc13
projectalcantara11 <projectalcantara11@live.com>
2014-04-16 19:39:54 +0800

    Partial checkin

--- Q2014_MagicTrick/Q2014_MagicTrick.cpp
+++ Q2014_MagicTrick/Q2014_MagicTrick.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Global variables.
+int nCase = 0;
+int nNumberOfLines = 0;
+bool bIsNameIn = false;
+string sNames[100];
+string sNameIn = "";
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cin >> nCase;
+	cin >> nNumberOfLines;
+	
+	for (int j = 0; j < nNumberOfLines; j++)
+	{
+		cin >> sNames[j];
+	}
+	for (int i = 0; i < nCase; i++)
+	{
+		cout << "Case#" << i + 1 << ":" << sNames[i] << endl;//sNameIn << endl;
+	}
+
 	return 0;
 }
 
