12207da38228ea73080ca1a9e42d9248967a8551
josephnicholas <josephnicholasalcantara@gmail.com>
2014-05-05 13:24:21 +0800

    TODO: String Comparison and output.

--- Q2014_ProbA_MagicTrick/Q2014_ProbA_MagicTrick.cpp
+++ Q2014_ProbA_MagicTrick/Q2014_ProbA_MagicTrick.cpp
@@ -5,70 +5,115 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 //Containers.
-vector<string> vCardArrangement;
+vector<string> vCardArrangement1;
+vector<string> vCardArrangement2;
+
 
 //variables.
 int nNumberOfCases{ 0 };
-int nRowNumber{ 0 };
+int nRowNumber1{ 0 };
+int nRowNumber2{ 0 };
 string sCardNumbers;
 
 //function prototype.
-string sFoundInRow(vector<string>, int);
+string sFoundInRow(vector<string>, vector<string>, int, int);
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	bool isFinshedArr1 = false;
 	cin >> nNumberOfCases;
 	for (int i = 0; i < nNumberOfCases; i++)
 	{
-		cin >> nRowNumber;
-		/***************************************************************
-
-		Stack overflow:
-		You have a different problem than the one described in the faq.
-		You are mixing formatted and unformatted input. formatted input
-		leaves trailing newlines in the stream. Your unformatted input,
-		getline, stops when it encounters a newline.
-
-		***************************************************************/
-
-		if (cin.peek() == '\n')
-			cin.ignore();
-
-		/***
-		-end-
-		***/
-		for (int j = 0; j < 4; j++)
+		for (int l = 0; l < 2; l++)
 		{
-			getline(cin, sCardNumbers);
-			vCardArrangement.push_back(sCardNumbers);
-		}
-		//Find specific card number by inputted row position.
-		for (int k = 0; k < vCardArrangement.size(); k++)
-		{
-			if ((vCardArrangement.size() % 4) == 0)
+			if (!isFinshedArr1)
 			{
-				cout << "\n";
-
+				cout << "nRowNumber1:";
+				cin >> nRowNumber1;
 			}
-			cout << vCardArrangement[k] << " ";
-		}
-		
+			else
+			{
+				cout << "nRowNumber2:";
+				cin >> nRowNumber2;
+			}
 
-		cout << "Case #"<<i + 1<<":"<<vCardArrangement.size() << endl;
+
+			if (cin.peek() == '\n')
+				cin.ignore();
+
+			if (isFinshedArr1)
+			{
+				cout << "Set2:\n";
+				sCardNumbers.clear();
+				for (int k = 0; k < 4; k++)
+				{
+					getline(cin, sCardNumbers);
+					vCardArrangement2.push_back(sCardNumbers);
+				}
+			}
+
+			if (!isFinshedArr1)
+			{
+				cout << "Set1:\n";
+				for (int j = 0; j < 4; j++)
+				{
+					if (j == 3) //4th cycle.
+					{
+						isFinshedArr1 = true;
+					}
+					getline(cin, sCardNumbers);
+					vCardArrangement1.push_back(sCardNumbers);
+				}
+			}
+
+			
+
+
+		}
+				
+		cout << "Output:" << sFoundInRow(vCardArrangement1, vCardArrangement2, nRowNumber1, nRowNumber2) << endl;
+		//cout << "Case #"<<i + 1<<":"<<vCardArrangement1.size() << endl;
 	}
 
 	return 0;
 }
 
 //Implementation.
-string sFoundInRow(vector<string> vNumberContainer, int nRow)
+string sFoundInRow(vector<string> vNumberContainer1, vector<string> vNumberContainer2, int nRowArr1, int nRowArr2)
 {
-	string sFoundNumber;
+	string sFoundNumber, sTemp1, sTemp2;
+	
+	sTemp1 = vNumberContainer1[nRowArr1 - 1];
+	sTemp2 = vNumberContainer2[nRowArr2 - 1];
+
+	sTemp1.erase(remove_if(sTemp1.begin(), sTemp1.end(), isspace), sTemp1.end());
+	sTemp2.erase(remove_if(sTemp2.begin(), sTemp2.end(), isspace), sTemp2.end());
+
+	cout << "\n";
+
+	cout << sTemp1 << endl;
+	cout << sTemp2 << endl;
+
+	sFoundNumber = sTemp1.compare(sTemp2);
+	sTemp1.
 
 	return sFoundNumber;
 }
+
+//Find specific card number by inputted row position.
+//for (int k = 0; k < vCardArrangement1.size(); k++)
+//{
+//	if ((vCardArrangement1.size() % 4) == 0)
+//	{
+//		cout << "\n";
+//
+//	}
+//	cout << vCardArrangement1[k] << " ";
+//}
+
